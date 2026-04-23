8e668f73e0b42cbac71b57940c2d7bdb0201cd29
Dhruva <dhruvatajapara@gmail.com>
2026-04-23 23:47:17 +0530

    feat: add create note endpoint (DELETE /api/notes/:id)

--- src/controllers/note.controller.js
+++ src/controllers/note.controller.js
@@ -229,6 +229,43 @@ const updateNote = async (req, res) => {
   }
 };
 
+const deleteNote = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!isValidId(id)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid ID",
+        data: null,
+      });
+    }
+
+    const deleted = await Notes.findByIdAndDelete(id);
+
+    if (!deleted) {
+      return res.status(404).json({
+        success: false,
+        message: "Note not found",
+        data: null,
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Note deleted successfully",
+      data: null,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      data: null,
+    });
+  }
+};
+
 
 module.exports = {
     createNote,
@@ -237,4 +274,5 @@ module.exports = {
     getNotesById,
     replaceNote,
     updateNote,
+    deleteNote,
 }
--- src/routes/note.routes.js
+++ src/routes/note.routes.js
@@ -8,6 +8,7 @@ const{
     getNotesById,
     replaceNote,
     updateNote,
+    deleteNote,
 } = require('../controllers/note.controller');
 
 router.post('/',createNote);
@@ -16,5 +17,6 @@ router.get('/',getAllNotes);
 router.get('/:id',getNotesById);
 router.put('/:id',replaceNote);
 router.patch('/:id',updateNote);
+router.delete('/:id',deleteNote);
 
 module.exports = router;
